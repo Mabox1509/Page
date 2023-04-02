94036680bb5b6d9a3802215a566745a1fc285c6c
Mabox1509 <73403310+Mabox1509@users.noreply.github.com>
2023-04-02 14:11:04 -0600

    Minecraft

---
+++ Code/minecraft.js
@@ -0,0 +1,28 @@
+const ip = "2.tcp.ngrok.io:11976";
+const url = "https://mcapi.us/server/status?ip=" + ip;
+
+// Función que copia la dirección IP al portapapeles del usuario
+function copyIp() {
+	const ipInput = document.getElementById("ip");
+	ipInput.select();
+	ipInput.setSelectionRange(0, 99999);
+	document.execCommand("copy");
+	alert("La dirección IP ha sido copiada al portapapeles.");
+}
+
+// Función que muestra la lista de usuarios conectados
+function showUserList(data) {
+	const userList = document.getElementById("userList");
+	if (data.status === "success" && data.players && data.players.sample) {
+		const users = data.players.sample.map(user => `<li>${user.name}</li>`).join("");
+		userList.innerHTML = users;
+	} else {
+		userList.innerHTML = "<li>No se encontraron usuarios conectados.</li>";
+	}
+}
+
+// Llamada a la API para obtener la información del servidor
+fetch(url)
+	.then(response => response.json())
+	.then(data => showUserList(data))
+	.catch(error => console.error(error));
